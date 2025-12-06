14c1eb81578e06ea4556f844cd13124215f7df36
pengbo <pengbo@hhh.com>
2025-12-06 11:58:27 +0800

    debug

--- sync_stock_minute.sh
+++ sync_stock_minute.sh
@@ -1,4 +1,4 @@
 # python stock_minute.py workers 16 period 5
 # python apply_stock_minute.py period 5
-python stock_minute.py workers 2 period 30
+python stock_minute.py workers 1 period 30
 python apply_stock_minute.py period 30
