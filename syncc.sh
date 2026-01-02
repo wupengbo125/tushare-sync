36daac9001b8bd3427d63ed13effc6f8114e9a21
pengbo <pengbo@hhh.com>
2026-01-02 17:42:34 +0800

    debug

--- syncc.sh
+++ syncc.sh
@@ -17,6 +17,6 @@ done
 
 python ths-hot-concept.py
 # python ths-hot-concept.py -d 20251202 --no-sync
-# python sync_stock_concept.py
+# python sync_stock_concept_akshare_em.py
 # python sync_stock_concept_adata_ths.py
 # python dividend_rank.py --workers 8 --allow-qfq-fallback
