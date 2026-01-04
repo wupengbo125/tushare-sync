f6b0c6ed8ed2c4bccf05d06f9ebd1d3ffa8b8e2c
wupengbo <wupengbo125@outlook.com>
2026-01-04 22:18:10 +0800

    debug

--- get_concept_ths.py
+++ get_concept_ths.py
@@ -1,3 +1,4 @@
 import adata
-df = adata.stock.info.get_concept_ths(stock_code="300033")
+# df = adata.stock.info.get_concept_ths(stock_code="300033")
+df = adata.sentiment.hot.hot_concept_20_ths()
 print(df)
